263bf2eae50bc036311d3efadf60226b3feec36b
unknown <hr9520763@gmail.com>
2021-12-09 13:04:47 +0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,300 @@
+
+        //CHAPTER NO 1 : ALERT
+// 1: alert("welcome");
+// 2: alert("Error! Please enter a valid password");
+// 3: alert("Welcome to JS Land...\nHappy Coding!");
+// 4: alert("Welcome to JS Land...");
+// alert("Happy Coding");
+
+
+
+         //CHAPTER NO 2: Variable for string
+// 1: var username = "Hasnain Raza";  
+// 2: var myName = "Hasnain Raza";  
+// 3: var messsage ="Hello world "; 
+// alert(messsage)   
+// var studentname = "Hasnain Raza";
+// var studentage = "18 years";
+// var studentskills = "Web developer";
+// 4: alert(studentname)
+// alert(studentage)
+// alert(studentskills)
+// 5: var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza)
+// 6: var e_line = "My email address is"
+// var Email = "hr9520763@gmail.com"
+// alert(e_line+Email)
+// 7: var bookname = "I am trying to learn a BOOK"
+// var book = "A smarter way to learn javascript"
+// alert(bookname+book)
+// 8:var doc = ("Yah! I can write HTML content in JAVASCRIPT")
+// document.write(doc)
+// var msg = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(msg)
+
+
+       
+          //CHAPTER NO 3:VARIABLE FOR NUMBER
+// 1: var age = "Iam 18 years old"
+// alert(age)
+// 2: var visited = " You have visited this site 14 times"
+// alert(visited)
+// 3: var birthyear = 2004
+// var mybirth = "My birth year is "
+// document.write(mybirth+birthyear)
+//4: var vistor = "Hasnain"
+// var product = "juices"
+// var quantity = "30"
+// document.write(vistor +"ordered" +quantity +product +"from xyz general store")
+
+
+
+            //CHAPTER NO 4:Variable Names: legal & illegal
+// 1:Do not declare more than one variable per declaration. Every declaration should be for a single variable, on its own line, with an explanatory comment about the role of the variable. Declaring multiple variables in a single declaration can cause confusion regarding the types of the variables and their initial values.
+// 2(a):    Here are the 5 legal variable 
+// 2(a1):var name            
+// 2(a2):var Name            
+// 2(a3):var $name            
+// 2(a4):var n12124           
+// 2(a5):var _name  
+
+//2(b):     Here are the 5 illegal variable
+// 2(b1):var 1name            
+// 2(b2):var -Name            
+// 2(b3):var (name            
+// 2(b4):var ,12124           
+// 2(b5):var \name
+
+// 3(a):document.write("Rules for Naming JS variables");
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+// 3(b):document.write("Variable names can only contain Numbers ,$ sign ,_ and Integers after a alphabets");
+// document.write("<br>");
+// 3(c):document.write("Variables must begin with a Small Alphabets ,$ sign ,_ sign and big letters");
+// document.write("<br>");
+// 3(d):document.write("Variable names are case sensitive");
+// document.write("<br>");
+// 3(e):document.write(" Variable names should not be JS Keywords");
+
+
+              //CHAPTER NO 5:MATH EXPRESSIONS 
+// 1:var no2 = 2             
+// var no9 = 9
+// var sum = no2+no9
+// document.write("Sum of 2 and 9 is"+sum)
+
+// 2: var no2 = 2             
+// var no9 = 9
+// var sum = no2-no9
+// document.write("Sum of 2 and 9 is"+sum)
+
+//2: var no2 = 2             
+// var no9 = 9
+// var sum = no2*no9
+// document.write("Sum of 2 and 9 is"+sum)
+
+// 2: var no2 = 2             
+// var no9 = 9
+//  var sum = no2/no9
+//  document.write("Sum of 2 and 9 is"+sum)
+
+//3(a)  var nothing =
+
+//  document.write("<br>")
+// var und = ("Value after variable declaration is");
+//3(b) document.write(und+nothing);
+// document.write("<br>")
+
+//3(c)   var no = 5
+
+//  var initial = ("Initial value is :")
+//3(d) document.write(initial+no)
+// document.write("<br>")
+
+//3(e) var nob = ++no
+// var increment = ("Value after increment is :")
+
+//3(f)  document.write(increment+nob)
+// document.write("<br>")
+
+//3(g) var add = 7+nob
+// var addition = ("Value after decrement is :")
+
+//3(h)  document.write(addition+add)
+// document.write("<br>")
+
+//3(i) var dec = --add
+// var decrement = ("Value after decrement is :")
+
+//3(j)  document.write(decrement+dec)
+// document.write("<br>")
+
+//3(k) var div = dec/3
+// var divide =("The remainder is :")
+// var remainder = div-4
+
+//3(l)  document.write(divide+remainder)
+
+//4 var cost = 600
+// var sold = cost*5
+// var total_sell = ("Total cost of buy 5 ticket to a movie is"+sold+"PKR")
+// document.write(total_sell)
+
+// 5 var Table_4 = ("Table of 4")
+// document.write(Table_4)
+// document.write("<br>")
+// var four = 4
+// var four1 = ("4*1=")
+// var fourone = four*1
+// document.write(four1+fourone)
+// document.write("<br>")
+
+// var four2 = ("4*2=")
+// var fourtwo = four*2
+// document.write(four2+fourtwo)
+// document.write("<br>")
+
+// var four3 = ("4*3=")
+// var fourthree = four*3
+// document.write(four3+fourthree)
+// document.write("<br>")
+
+// var four4 = ("4*4=")
+// var fourfour = four*4
+// document.write(four4+fourfour)
+// document.write("<br>")
+
+// var four5 = ("4*5=")
+// var fourfive = four*5
+// document.write(four5+fourfive)
+// document.write("<br>")
+
+// var four6 = ("4*6=")
+// var foursix = four*6
+// document.write(four6+foursix)
+// document.write("<br>")
+
+// var four7 = ("4*7=")
+// var fourseven = four*7
+// document.write(four7+fourseven)
+// document.write("<br>")
+
+// var four8 = ("4*8=")
+// var foureight = four*8
+// document.write(four8+foureight)
+// document.write("<br>")
+
+// var four9 = ("4*9=")
+// var fournine = four*9
+// document.write(four9+fournine)
+// document.write("<br>")
+
+// var four10 = ("4*10=")
+// var fourten = four*10
+// document.write(four10+fourten)
+// document.write("<br>")
+ 
+//6(a)  var C = 10
+//  var cinotf =(C * 9/5) +32
+//6(b) document .write(C+"°C"+"is"+cinotf+"°F")
+// document.write("<br>")
+//6(c)  var F = 10
+//  var finotfc =(F- 32) *5/9
+//6(d)  document .write(F+"°F"+"is"+finotfc+"°C")
+
+//7(a): var  Priceofitem1 = 102
+//7(b): var  Priceofitem2 = 1050
+//7(c): var  Quantityofitem1 = 2
+//7(d): var  Quantityofitem2 = 10                               
+//7(e): var  shippingcharges = 200
+
+// document.write("Price of item 1 is:"+Priceofitem1)
+// document.write("<br>")
+// document.write("Quantity of item 1 is:"+Quantityofitem1)
+// document.write("<br>")
+// document.write("Price of item 2 is:"+Priceofitem2)
+// document.write("<br>")
+// document.write("Quantity of item 2 is:"+Quantityofitem2)
+// document.write("<br>")
+// document.write("Shipping Charges:"+shippingcharges)
+// document.write("<br>")
+// document.write("Toatl cost of your order is"+(Priceofitem1*Quantityofitem1+Priceofitem2*Quantityofitem2+shippingcharges))
+
+//8: var totalmarks = 100
+// var obtainmarks = 79
+// var percentage = obtainmarks/totalmarks*100
+// document.write("<h1>MARK SHEET</h1>")
+// document.write("Total Marks:100")
+// document.write("<br>")
+// document.write("Obatin Marks:79")
+// document.write("<br>")
+// document.write("Percentage:"+percentage+"%")
+
+//9: var dollar = 104.80 
+// var saudi = 28  
+// var total$ = dollar*10
+// var totals = saudi*25
+// var inpkr = total$+totals
+// document.write("<h1>CURRENCY IN PKR</h1>")
+// document.write("Total currency in PKR:"+inpkr)
+
+//10: var no = 7+5*10/2
+// document.write(no)
+
+//11: document.write("<h1>AGE CALCULATOR</h1>")
+// var currentyear = 2021
+// var birthdayyear = 2004
+// var age = currentyear-birthdayyear
+// document.write("Current Year:"+currentyear)
+// document.write("<br>")
+// document.write("Birth Year:"+birthdayyear)
+// document.write("<br>")
+// document.write("Your age is :"+age)
+
+//12(a) var radius = 20
+//12(b) var circumferance = radius*2*3.14
+//  document.write("<h1>The Geometizer</h1>")
+//  document.write("The Radius of circle is:"+radius)
+// document.write("<br>")
+// document.write("The Circumferance of circle is:"+circumferance)
+// var area = 3.14*20*20
+// document.write("<br>")
+// document.write("The area of circle is:"+area)
+
+//13(a) var favsnack = "lays"
+//13(b) var Currentage = 17
+//13(c) var Maxage = 65
+//13(d) var estamount = 3
+// document.write("<h1>The Lifetime Supply Calculator</h1>")
+// document.write("<br>")
+// document.write("Favorite snack:"+favsnack)
+// document.write("<br>")
+// document.write("Current age:"+Currentage)
+// document.write("<br>")
+// document.write("Estimated Maximum Age:"+Maxage)
+// document.write("<br>")
+// document.write("Amount of snacks per day:"+estamount)
+// document.write("<br>")
+// document.write("You will need 150 to last you until the ripe old age of 65")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
